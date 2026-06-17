ba6ac1962e25c12e032af1a0ccf895526694284f
Taesoo Kim <tsgatesv@gmail.com>
2026-06-17 13:05:04 -0400

    cask: bump to 0.2.0

--- Casks/redbull.rb
+++ Casks/redbull.rb
@@ -1,9 +1,9 @@
 cask "redbull" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.1.0"
-  sha256 arm:   "76b4ba7066804c0eba86d81c8306780484869761a6727691273c4b317e9802c9",
-         intel: "a1f185b13278e06d434f4735a1894db335dbb8cc44014b74d01005df3d4f425e"
+  version "0.2.0"
+  sha256 arm:   "2ea86fd987c8f1292ff08ed1a07fc036ba2906ea1844fb8e8d3566316432cda9",
+         intel: "b12081881448504d811c9b6c8176762d4304900f7eca3ac5d901d034c5250cd1"
 
   url "https://github.com/tsgates/redbull/releases/download/v#{version}/Redbull-#{version}-#{arch}.dmg"
   name "Redbull"
